7638db420451001dd9553c637f36f07b23e8e293
jchezum <josh.chezum@gmail.com>
2015-12-15 17:21:46 -0800

    Added foundational class code and related infrastructure

--- FifthAdvent.py
+++ FifthAdvent.py
@@ -1,5 +1,22 @@
+import re
 
+class FifthAdvent():
+	def __init__(self):
+		pass
 
+	def inputFile(self):
+		file = open('Input\FifthAdventInput','r')
+		unsortedWords=file.readlines()
+		file.close()
+		return unsortedWords
+
+	def main(self):
+		wordList=[]
+		wordList=self.inputFile()
+		print wordList
+
+fa=FifthAdvent()
+fa.main()
 
 
 # (.*[aeiou].*[aeiou].*[aeiou].*)
